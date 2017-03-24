a70c5e44d415e7d7706d87e623d72fca203f81fb
Amy Wu <jiaxuan@brandeis.edu>
2017-03-23 21:37:04 -0400

    tee iteration1

---
+++ exercises/ex03/tee.c
@@ -0,0 +1,33 @@
+/* 
+Author: Amy Wu 
+Purpose: Assignment3 Olin College Software Systems course
+Functionality: Program is an implementation of tee command
+Assumptions: I implemented without any options
+	
+Answer to question 3: What slowed me down was trying to fully understand what UNIX 		command tee does. Next time, I would probably try to understand every component that goes into the code before I start coding. I felt I didn't have a clear strategy.
+
+Answer to question 4: Professional code looks much easier to understand. For example, their refactoring is great. 
+ 
+To run: gcc tee.c -o tee && ./tee
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+/*
+Tee implementation of writing to files and standard output
+*/
+int main(int argc, char *argv[]){
+	char string1[4];
+	strcpy(string1, "tee");
+	
+	for(int i=0; i<argc;i++){
+		if(strncmp(string1, argv[i], 4)==0){
+			fprintf(stdout, argv[i++]);
+			FILE *file1 = fopen(argv[i++], "w");
+			fclose(file1);	
+		}		
+	}
+	
+	return 0;
+}
