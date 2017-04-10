b8e2853e6bb22d36c34e503f0354395d971030f7
Amy Wu <jiaxuan@brandeis.edu>
2017-04-10 13:58:40 -0700

    adding comment

--- exercises/ex08/ex_compile.c
+++ exercises/ex08/ex_compile.c
@@ -1,7 +1,11 @@
+
 /* Simple example using Glib.
-
 From http://www.ibm.com/developerworks/linux/tutorials/l-glib/
+*/
 
+/* 
+Compile: gcc -Wall -o ex-compile ex-compile.c $(pkg-config --cflags --libs glib-2.0)
+Run: ./ex-compile
 */
 
 #include <stdio.h>
@@ -14,4 +18,3 @@ int main(int argc, char** argv) {
     printf("The first item is '%s'\n", (char *) g_list_first(list)->data);
     return 0;
 }
-
