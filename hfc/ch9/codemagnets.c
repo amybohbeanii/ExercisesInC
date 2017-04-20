666becb3a86e7f850cbae71eced1ca33bd47d7a3
Amy Wu <jiaxuan@brandeis.edu>
2017-04-19 21:16:08 -0400

    Create codemagnets.c

---
+++ hfc/ch9/codemagnets.c
@@ -0,0 +1,20 @@
+/*program that writes timestamped text to end of a logfile*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char * now(){
+  time_t t;
+  time (&t);
+  return asctime(localtime (&t));
+}
+
+int main(){
+  char comment[80];
+  char cmd[120];
+  fgets(comment,80,stdin); //getting unstructured text from standard input and stores in comment array
+  sprintf(cmd,"echo '%s %s' >> reports.log",comment,now()); //prints characters to string. First comment, then timestamp stored in cmd array
+  system(cmd); //runs contents of cmd string
+  return 0;
+}  
+  
