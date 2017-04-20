b53433d012e5f42c64772d2e87fc9fedb1051163
Amy Wu <jiaxuan@brandeis.edu>
2017-04-19 23:43:49 -0400

    Update newshound.c

--- hfc/ch9/newshound.c
+++ hfc/ch9/newshound.c
@@ -12,6 +12,8 @@ int main (int argc, char *argv[]){
 	int times = 3;
 	char *phrase = argv[1];
 	int i;
+	/*the loop only runs once. Once execle() is called, the whole program quits*/
+	/*if you want to start another process and keep origin process running, use fork()*/
 	for(i=0;i<times;i++){
 		char var[255];
 		sprintf(var,"RSS_FEED=%s", feeds[i]);
