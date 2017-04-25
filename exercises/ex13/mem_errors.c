9d0cbde78c45578f0a78d273526375f14e029dc0
Amy Wu <jiaxuan@brandeis.edu>
2017-04-24 21:12:03 -0400

    Update mem_errors.c

--- exercises/ex13/mem_errors.c
+++ exercises/ex13/mem_errors.c
@@ -3,24 +3,31 @@
 Copyright 2014 Allen Downey
 License: Creative Commons Attribution-ShareAlike 3.0
 
+Modified by: Jiaxuan (Amy) Wu
+Date 4/24/17
+
+Learning Goal: Fix the errors using valgrind
+
+gcc -g -O0 mem_errors.c -o memerrors
+valgrind --leak-check=yes ./memerrors
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-
+/*
 void free_anything(int *p) {
     free(p);
 }
-
+*/
 
 int read_element(int *array, int index) {
     int x = array[index];
     return x;
 }
 
-
+/*Reorganized main to catch memory errors and leaks*/
 int main ()
 {
     int never_allocated;
@@ -31,28 +38,29 @@ int main ()
     int *array2 = malloc (100 * sizeof (int));
 
     // valgrind does not bounds-check static arrays
-    read_element(array1, -1);
-    read_element(array1, 100);
+    read_element(array1, 0);
+    read_element(array1, 99);
 
     // but it does bounds-check dynamic arrays
-    read_element(array2, -1);
-    read_element(array2, 100);
+    read_element(array2, 0);
+    read_element(array2, 99);
+    free(array2);
 
     // and it catches use after free
-    free(use_after_free);
     *use_after_free = 17;
-    
+    free(use_after_free);
+
     // never_free is definitely lost
     *never_free = 17;
-
+    free(never_free);
     // the following line would generate a warning
     // free(&never_allocated);
 
     // but this one doesn't
-    free_anything(&never_allocated);
+	// free_anything(&never_allocated);
     
     free(free_twice);
-    free(free_twice);
+	// free(free_twice);
 
     return 0;
 }
